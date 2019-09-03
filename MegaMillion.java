fdeb5c7b9d1aca6d6e5374081d7594944c8961c1
lovefchip <54686639+lovefchip@users.noreply.github.com>
2019-09-03 16:51:28 -0400

    Add files via upload

---
+++ MegaMillion.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @author Abraham
+ * description:
+ * 
+ */
+//import the Scanner class
+public class MegaMillion {
+	public static void main(String[] args) {
+		Random randGen= new Random();
+		int rnum1=randGen(randGen.nextInt(60)+1);
+		System.out.println(.
+				args
+		
+	// Declare Variable to hold amount of dollar
+		//Declare Variable for cost of ticket
+		
+		//Prompt the user for the amount
+		// of dollars to bet on
+		
+		//store amount into amount of dollar
+		// variable
+		
+		//Prompt the user to enter six numbers
+		// create a variable for each value
+		
+		//Randomly generate
+		
+	}
+	}
+
