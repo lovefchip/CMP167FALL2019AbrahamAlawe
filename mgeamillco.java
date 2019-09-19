f582f518c30e5d1ec24e7d2fee2f52c96c2cb108
lovefchip <54686639+lovefchip@users.noreply.github.com>
2019-09-19 16:42:35 -0400

    Add files via upload

---
+++ mgeamillco.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @author Abraham
+ *
+ */
+public class Megamillion {
+
+	import java.util.Scanner;
+	      public static void main(String[] args) {
+	    	  
+	    	  int ticket;
+	    	  int dollar;
+	    	  
+	    	 Scanner in = new Scanner(System.in);
+	    	 System.out.println("This program will generate a random number ")
+	    	 
+	    	 
+	    	  
+	    	  
+	    	  
+	    	  
+	      }
+}
+}      
+	      
