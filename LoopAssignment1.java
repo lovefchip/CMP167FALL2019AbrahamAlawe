b369b798d1f7fc147ff6a5816c958098bac6aaeb
lovefchip <54686639+lovefchip@users.noreply.github.com>
2019-10-03 16:42:11 -0400

    Add files via upload
    not yet finished

---
+++ LoopAssignment1.java
@@ -0,0 +1,17 @@
+
+
+	
+public class LoopAssignment1 {
+
+	public static void main(String[] args)
+	{
+	
+				
+	int n = 50;
+		for(int i =0;i < 150;i++);
+		
+		if(n%3==0);
+		
+		System.out.println("it "+ "is even");
+	}
+}
